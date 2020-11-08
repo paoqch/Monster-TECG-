e392ae54a0f2ae5af706219b2855369477834929
Paola Quirós <66766320+paoqch@users.noreply.github.com>
2020-11-08 01:05:05 -0600

    Update PoderDivinoHechizo.java

--- Monster-TECG-Project/src/cards/spells/PoderDivinoHechizo.java
+++ Monster-TECG-Project/src/cards/spells/PoderDivinoHechizo.java
@@ -1,5 +1,9 @@
 package cards.spells;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import cards.Card;
 import cards.MonsterCard;
 
 public class PoderDivinoHechizo extends SpellCard {
@@ -11,12 +15,22 @@ public class PoderDivinoHechizo extends SpellCard {
 	}
 
 	public void action(MonsterCard monster) {
+		Random r = new Random();
+		int increase = 100 * (r.nextInt(10) + 1);
 
-		int monsterCardsCount = ArrepentimientoHechizo.getBoard().getActivePlayer().getField().getMonstersArea().size();
-		
-		monster.setAttackPoints(monster.getAttackPoints()
-				+ (150 * monsterCardsCount));
+		ArrayList<MonsterCard> monsters = Card.getBoard().getActivePlayer()
+				.getField().getMonstersArea();
+		for (int i = 0; i < monsters.size(); i++) {
+
+			monsters.get(i).setAttackPoints(
+					monsters.get(i).getAttackPoints() + increase);
+			monsters.get(i).setDefensePoints(
+					monsters.get(i).getDefensePoints() + increase);
 
 		}
 
+	}
+
+		
 }
+
