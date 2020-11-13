7833ddd653e1e10c19ddcb982a6b64b7c79c4445
Paola Quirós <66766320+paoqch@users.noreply.github.com>
2020-11-12 23:31:00 -0600

    Update MonsterButton.java

--- Monster-TECG-Project/src/gui/MonsterButton.java
+++ Monster-TECG-Project/src/gui/MonsterButton.java
@@ -5,24 +5,33 @@ import javax.swing.JButton;
 import cards.MonsterCard;
 
 public class MonsterButton extends JButton {
-private MonsterCard monster;
-	public MonsterCard getMonster() {
-	return monster;
-}
-public void setMonster(MonsterCard monster) {
-	this.monster = monster;
-}
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private MonsterCard monster;
+		public MonsterCard getMonster() {
+		return monster;
+	}
+		
+	public void setMonster(MonsterCard monster) {
+		this.monster = monster;
+	}
+	
 	public MonsterButton() {
-		this.setVisible(true);
+			this.setVisible(true);
 	}
+	
 	public MonsterButton(MonsterCard monster){
-		this.setVisible(true);
-		this.setName(monster.getName());
-		
+			this.setVisible(true);
+			this.setName(monster.getName());
+			
 	}
+	
 	public MonsterButton(String name){
-		
-		super(name);
+			
+			super(name);
 	}
 
 }
